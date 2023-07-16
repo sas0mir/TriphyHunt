2c5270dabf226c5a7abf33fd555f8dc0ac54e19b
sas_mir <sas_mir@icloud.com>
2023-07-16 16:44:16 +0200

    Initial Commit

---
+++ TriphyHunt/TriphyHuntApp.swift
@@ -0,0 +1,17 @@
+//
+//  TriphyHuntApp.swift
+//  TriphyHunt
+//
+//  Created by Станислав Смирнов on 16.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TriphyHuntApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
